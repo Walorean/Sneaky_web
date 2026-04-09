613cd5c06a1f5f68dd3536c22c9e4d10c4478cb5
artemKinash <126754117+ArtemKinash47@users.noreply.github.com>
2026-04-09 12:41:23 +0200

    layouts

--- faza_2/sneakyzz_laravel/resources/views/index.blade.php
+++ faza_2/sneakyzz_laravel/resources/views/index.blade.php
@@ -1,41 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>SNEAKYZZ</title>
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
-</head>
-<body>
-<section class="header_container">
-    <header class="header">
-        <a href="{{route('home')}}"><h1>SNEAKYZZ</h1></a>
-        <div class = "search_box">
-            <input type="text" id="search" placeholder="Search product...">
-            <button id="search_button">🔍</button>
-        </div>
-        <div class="main_buttons">
-            <button id="likes" onclick="location.href='liked_page.html'">❤️</button>
-            <button id="cart" onclick="location.href='shopping_cart.html'">🛒</button>
-            @auth
-                <button id="my_profile" onclick="window.location='{{ route('my_profile') }}'">👤</button>
-            @endauth
-
-            @guest
-                <button id="my_profile" onclick="window.location='{{ route('login') }}'">👤</button>
-            @endguest
-        </div>
-    </header>
-    <nav class="navbar">
-        <div class="navbar_buttons">
-            <button onclick="location.href='category_products_page.html'"><strong>NEW</strong></button>
-            <button onclick="location.href='category_products_page.html'"><strong>MEN</strong></button>
-            <button onclick="location.href='category_products_page.html'"><strong>WOMEN</strong></button>
-            <button onclick="location.href='category_products_page.html'"><strong>SPORT</strong></button>
-        </div>
-    </nav>
-</section>
-<section class="main_container">
+@extends('layout.layout_customer')
+@section('content')
     <div class="hero-ad">
         <div class="banner">
             <button class="arrows left-arrow"><</button>
@@ -49,7 +13,7 @@
     </div>
     <h1>FOR MEN</h1>
     <section id="men_main">
-        <img id="men_main_picture" src="{{ Vite::asset('resources/assets/sneaker_men.png') }}">
+        <img id="men_main_picture" src="{{ Vite::asset('resources/assets/sneaker_men.png') }}" alt="">
         <div class="products">
             <div class="product-card" onclick="location.href='product_page.html'">
                 <div class="product-image-box">
@@ -129,40 +93,6 @@
                 </div>
             </div>
         </div>
-        <img id="women_main_picture" src="{{ Vite::asset('resources/assets/sneaker_men.png') }}">
+        <img id="women_main_picture" src="{{ Vite::asset('resources/assets/sneaker_men.png') }}" alt="">
     </section>
-</section>
-<section class="footer_container">
-    <footer class="footer">
-        <div id="about">
-            <h3>ABOUT US</h3>
-            <ul>
-                <li><strong>Yevhen Horschar - FIIT student</strong></li>
-                <li><strong>Artem Kinash - FIIT student</strong></li>
-            </ul>
-        </div>
-        <div id="resource">
-            <h3>RESOURCE</h3>
-            <ul>
-                <li><strong>BLOG</strong></li>
-                <li><strong>NEWS</strong></li>
-            </ul>
-        </div>
-        <div id="help">
-            <h3>HELP</h3>
-            <ul>
-                <li><strong>xhorshchar@stuba.sk</strong></li>
-                <li><strong>xkinash@stuba.sk</strong></li>
-            </ul>
-        </div>
-        <div id="important-info">
-            <h3>IMPORTANT INFORMATION</h3>
-            <ul>
-                <li><strong>WE CREATED OUR MAIN TITLE</strong></li>
-                <li><strong>WE CREATED LOGIN, REGISTER AND MY PROFILE</strong></li>
-            </ul>
-        </div>
-    </footer>
-</section>
-</body>
-</html>
+@endsection
---
+++ faza_2/sneakyzz_laravel/resources/views/layout/layout_customer.blade.php
@@ -0,0 +1,75 @@
+﻿<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SNEAKYZZ</title>
+    @vite(['resources/css/app.css', 'resources/js/app.js', 'resources/css/profile.css'])
+    @stack('styles')
+</head>
+<body>
+<section class="header_container">
+    <header class="header">
+        <a href="{{route('home')}}"><h1>SNEAKYZZ</h1></a>
+        <div class = "search_box">
+            <input type="text" id="search" placeholder="Search product...">
+            <button id="search_button">🔍</button>
+        </div>
+        <div class="main_buttons">
+            <button id="likes" onclick="location.href='liked_page.html'">❤️</button>
+            <button id="cart" onclick="location.href='shopping_cart.html'">🛒</button>
+            @auth
+                <button id="my_profile" onclick="window.location='{{ route('my_profile') }}'">👤</button>
+            @endauth
+
+            @guest
+                <button id="my_profile" onclick="window.location='{{ route('login') }}'">👤</button>
+            @endguest
+        </div>
+    </header>
+    <nav class="navbar">
+        <div class="navbar_buttons">
+            <button onclick="location.href='category_products_page.html'"><strong>NEW</strong></button>
+            <button onclick="location.href='category_products_page.html'"><strong>MEN</strong></button>
+            <button onclick="location.href='category_products_page.html'"><strong>WOMEN</strong></button>
+            <button onclick="location.href='category_products_page.html'"><strong>SPORT</strong></button>
+        </div>
+    </nav>
+</section>
+<main class="main_container">
+    @yield('content')
+</main>
+<section class="footer_container">
+    <footer class="footer">
+        <div id="about">
+            <h3>ABOUT US</h3>
+            <ul>
+                <li><strong>Yevhen Horschar - FIIT student</strong></li>
+                <li><strong>Artem Kinash - FIIT student</strong></li>
+            </ul>
+        </div>
+        <div id="resource">
+            <h3>RESOURCE</h3>
+            <ul>
+                <li><strong>BLOG</strong></li>
+                <li><strong>NEWS</strong></li>
+            </ul>
+        </div>
+        <div id="help">
+            <h3>HELP</h3>
+            <ul>
+                <li><strong>xhorshchar@stuba.sk</strong></li>
+                <li><strong>xkinash@stuba.sk</strong></li>
+            </ul>
+        </div>
+        <div id="important-info">
+            <h3>IMPORTANT INFORMATION</h3>
+            <ul>
+                <li><strong>WE CREATED OUR MAIN TITLE</strong></li>
+                <li><strong>WE CREATED LOGIN, REGISTER AND MY PROFILE</strong></li>
+            </ul>
+        </div>
+    </footer>
+</section>
+</body>
+</html>
